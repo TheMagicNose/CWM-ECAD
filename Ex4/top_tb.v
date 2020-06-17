88704208efb06377b4bd733be6dbca995c15e575
Cloud User <centos@ip-172-31-6-222.eu-west-2.compute.internal>
2020-06-17 15:34:47 +0000

    An incomplete solution Ex4

--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -6,4 +6,42 @@
 // Description: A testbench module to test Ex4 - Electronic Dice
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+timescale 1ns / 100ps
+
+module top_tb(
+);
+
+parameter CLK_PERIOD = 10;
+
+reg clk;
+reg button;
+reg rst;
+reg err;
+wire [2:0] throw;
+reg [2:0] throw_prev;
+
+initial
+begin
+clk = 1'b0;
+forever
+#(CLK_PERIOD/2) clk!=clk;
+end
+
+initial 
+begin
+rst = 0;
+button = 1;
+err = 1;
+#CLK_PERIOD rst = 0;
+clk=0;
+
+forever begin
+
+#CLK_PERIOD if (rst&(throw!=0))
+begin
+$display("***TEST FAILED! Reset not working");
+err = 1;
+end 
+
+
 
