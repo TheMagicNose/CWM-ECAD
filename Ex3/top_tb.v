c3afffaa10cf306b5fb3683554f3be716e884851
Cloud User <centos@ip-172-31-6-222.eu-west-2.compute.internal>
2020-06-17 14:41:36 +0000

    A complete solution Ex3

--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -22,13 +22,13 @@ reg clk;
 reg direction;
 reg enable;
 reg rst;
-reg [7:0] counter_out;
+wire [7:0] counter_out;
 reg [7:0] counter_out_prev;
 
 
 //Todo: Clock generation
- initial
-    begin
+ initial begin
+    
        clk = 1'b0;
        forever
          #(CLK_PERIOD/2) clk=~clk;
@@ -37,14 +37,68 @@ reg [7:0] counter_out_prev;
 //Todo: User logic
 
 initial begin
-err=0;
-enable=0;
-rst=1;
-clk=0;
-direction=1;
+err = 0;
+enable = 0;
+rst = 1;
+clk = 0;
+direction = 1;
+
+#((CLK_PERIOD/2)+1)
 
 forever begin 
- 
+
+#(CLK_PERIOD-((CLK_PERIOD/2)+1))
+
+if (counter_out != counter_out_prev)
+begin 
+$display("***TEST FAILED! did not maintain 5 ticks gap!");
+err = 1;
+end
+
+#((CLK_PERIOD/2)+1)
+if ((enable&&(counter_out==counter_out_prev))|(!enable&&(counter_out!=counter_out_prev)))
+begin
+$display("***TEST FAILED! Enable is the wrong value");
+err = 1;
+end 
+
+
+if ((direction&&(counter_out!=counter_out_prev+1))|(!direction&&(counter_out!=counter_out_prev-1)))
+begin
+$display("***TEST FAILED! Direction is wrong");
+err = 1;
+end 
+
+
+if (rst&&(counter_out!=0))
+begin
+$display("***TEST FAILED! Reset isn't working");
+err = 1;
+end 
+
+counter_out_prev = counter_out;
+
+if (counter_out==8'b00000011)
+direction = 0;
+
+if ((direction==0) & (counter_out==8'b00000001))
+rst = 1;
+
+if (rst==1)
+direction = 1;
+
+if (counter_out==8'b00000000)
+rst = 0;
+
+if (enable==0)
+enable = 1;
+
+end
+
+end
+
+
+
 
 
     
@@ -57,5 +111,12 @@ forever begin
       end
 
 //Todo: Instantiate counter module
+
+counter top(
+.rst (rst),
+.clk (clk),
+.direction (direction),
+.enable (enable),
+.counter_out (counter_out));
  
 endmodule 
