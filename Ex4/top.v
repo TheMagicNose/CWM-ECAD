aaef3f72ab1c1446256bdd57de266f69c6487732
Cloud User <centos@ip-172-31-6-222.eu-west-2.compute.internal>
2020-06-18 09:10:48 +0000

    Test bench not working

--- Ex4/top.v
+++ Ex4/top.v
@@ -18,20 +18,20 @@
  `timescale 1ns / 100ps
  
  module dice(
+   input clk,
    input rst,
-   input clc,
    input button,
    output reg [2:0] throw
  ); 
 
 
-always@(posedge clk) begin
+always @(posedge clk) begin
 
 if(rst)
-throw <= 3'b00;
+throw <= 3'b000;
 
 else begin
-if ((throw == 3'b00) | (throw=3'b111))
+if ((throw == 3'b000) | (throw == 3'b111))
 throw <= 3'b001;
 
 if (throw < 3'b110)
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -20,28 +20,83 @@ reg err;
 wire [2:0] throw;
 reg [2:0] throw_prev;
 
-initial
-begin
+initial begin
+
 clk = 1'b0;
 forever
-#(CLK_PERIOD/2) clk!=clk;
+#(CLK_PERIOD/2) clk =! clk;
 end
 
-initial 
-begin
-rst = 0;
+initial begin
+
+rst = 1;
 button = 1;
-err = 1;
+err = 0;
 #CLK_PERIOD rst = 0;
 clk=0;
 
 forever begin
 
-#CLK_PERIOD if (rst&(throw!=0))
+#CLK_PERIOD if (rst&(throw != 0))
 begin
 $display("***TEST FAILED! Reset not working");
 err = 1;
+end
+
+if((throw == 0) | (throw == 3'b111) & (rst!=0))
+begin
+$display("***TEST FAILED! Impossible without reset");
+err = 1;
+end
+
+else
+
+begin
+if(throw_prev != 3'b110)
+
+begin
+if((button&(throw != throw_prev+1)) | (!button&(throw != throw_prev)))
+begin
+$display("***TEST FAILED! Button not working");
+err = 1;
+end
+end
+
+else
+
+begin
+if((button&(throw != 3'b001)) | (!button&(throw != throw_prev)))
+begin
+$display("***TEST FAILED! Button not working");
+err = 1;
+end
+end
+end
+
+throw_prev = throw;
+button= !button;
+
 end 
+end
+
+initial begin
+        #CLK PERIOD 
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+
+dice top(
+.clk (clk)
+.rst (rst)
+.button (button)
+.throw (throw)
+);
+
+end module
+
+
+ 
 
 
 
